43d6daa5cefa8bf49c6a8a79dc4175fe3ea81a6a
Damian Gleumes <damgem@zoho.com>
2021-02-10 20:10:24 +0100

    added navbar styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,18 +2,39 @@
 import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 
+function SectionLink(props) {
+
+    var style = {
+        transform: 'skew(-30deg)',
+        marginLeft: (props.base? '-20px':'-1px'),
+        marginRight: '-1px',
+        marginTop: '-1px',
+        paddingLeft: (props.base? '33px':'14px'),
+        paddingRight: '14px'
+    }
+
+    return (
+        <Nav.Link href={props.href} style={style}>
+            <p style={{transform: 'skew(30deg)'}} className='mb-0 secondary-font'>
+                {props.children}
+            </p>
+        </Nav.Link>
+    );
+}
+
 function Navigation() {
     return (
         <div className="slanted-outer" style={{zIndex:10, position: 'fixed', top: 0, left: 0}}>
-            <Navbar expand="sm">
+            <Navbar expand="sm" className="p-0">
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Nav.Link href="#start">Damian Gleumes</Nav.Link>
+                <SectionLink href="#start" base>Damian Gleumes</SectionLink>
+
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav>
-                        <Nav.Link href="#projects">Projects</Nav.Link>
-                        <Nav.Link href="#softskills">Soft Skills</Nav.Link>
-                        <Nav.Link href="#hardskills">Hard Skills</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
+                        <SectionLink href="#projects">Projects</SectionLink>
+                        <SectionLink href="#softskills">Soft Skills</SectionLink>
+                        <SectionLink href="#hardskills">Hard Skills</SectionLink>
+                        <SectionLink href="#contact">Contact</SectionLink>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
